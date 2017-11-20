8d0cf62fdc3cafa906da4670cce7e6bab90de2e6
喜欢、陪你看风景 <programmer_zzq@163.com>
2017-11-20 21:01:03 +0800

    update

--- app/src/main/java/com/coder/zzq/smartshowdemo/GithubToastTestActivity.java
+++ app/src/main/java/com/coder/zzq/smartshowdemo/GithubToastTestActivity.java
@@ -22,26 +22,26 @@ public class GithubToastTestActivity extends AppCompatActivity {
 
     public void onDefaultClick(View view) {
         //在默认位置显示
-        SmartToast.show("荔枝！");
+        SmartToast.show("荔枝");
     }
 
     public void onTopClick(View view) {
         //在屏幕顶部显示，距离顶部位置为Toast在Y方向默认的偏移距离
-        SmartToast.showAtTop("苹果！");
+        SmartToast.showAtTop("苹果");
     }
 
     public void onCenterClick(View view) {
         //在屏幕中央显示
-        SmartToast.showInCenter("香蕉！");
+        SmartToast.showInCenter("香蕉");
     }
 
     public void onCustomClick(View view) {
         //在指定位置显示，x,y方向偏移量单位为dp
-        SmartToast.showAtLocation("芒果！", Gravity.LEFT | Gravity.TOP,10,10);
+        SmartToast.showAtLocation("芒果", Gravity.LEFT | Gravity.TOP,10,10);
     }
 
 
     public void onContentChangedClick(View view) {
-        SmartToast.show("桔子！");
+        SmartToast.show("桔子");
     }
 }
