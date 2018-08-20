31f186a7d5742859d2ea75ba932ee5ba1fb34fc8
zzq <coder_zzq@163.com>
2018-08-20 15:42:41 +0800

    更新文档

--- app/src/main/java/com/coder/zzq/smartshowdemo/MyApplication.java
+++ app/src/main/java/com/coder/zzq/smartshowdemo/MyApplication.java
@@ -22,30 +22,30 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         SmartShow.init(this);
-//        SmartShow.toastSetting()
-//                //自定义布局，参数可以是布局资源，也可以View。
-//                // 在自定义布局中，一定要设置显示文本提示的
-//                //TextView的Id为android:id="@id/custom_toast_msg"。
+        SmartShow.toastSetting()
+                //自定义布局，参数可以是布局资源，也可以View。
+                // 在自定义布局中，一定要设置显示文本提示的
+                //TextView的Id为android:id="@id/custom_toast_msg"。
 //                .view(R.layout.custom_toast)
-//                //设置背景颜色
-//                .backgroundColorRes(R.color.colorPrimary)
-//                //文本颜色
-//                .textColorRes(R.color.colorAccent)
-//                //设置文本字体大小
-//                .textSizeSp(18)
-//                //设置文本是否加粗
-//                .textBold(true)
-//                //设置离开当前页面时，该页面的Toast是否立即消失
-//                .dismissOnLeave(true)
-//                //对布局进一步处理
-//                .processView(new ProcessViewCallback() {
-//                    @Override
-//                    //isCustom 是否是自定义布局；rootView 布局根view；outParent 默认布局时，msgView的父布局，也是根布局
-//                    //msgView 显示文本的TxtView
-//                    public void processView(boolean isCustom, View rootView, LinearLayout outParent, TextView msgView) {
-//                        //...
-//                    }
-//                });
+                //设置背景颜色
+                .backgroundColorRes(R.color.colorPrimary)
+                //文本颜色
+                .textColorRes(R.color.colorAccent)
+                //设置文本字体大小
+                .textSizeSp(18)
+                //设置文本是否加粗
+                .textBold(true)
+                //设置离开当前页面时，该页面的Toast是否立即消失
+                .dismissOnLeave(true)
+                //对布局进一步处理
+                .processView(new ProcessViewCallback() {
+                    @Override
+                    //isCustom 是否是自定义布局；rootView 布局根view；outParent 默认布局时，msgView的父布局，也是根布局
+                    //msgView 显示文本的TxtView
+                    public void processView(boolean isCustom, View rootView, LinearLayout outParent, TextView msgView) {
+                        //...
+                    }
+                });
 
         SmartShow.snackbarSetting()
                 //设置背景颜色
