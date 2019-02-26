2447ba206a6d89b78d32c5e682285f461957b9df
zhuzhiqiang <zhiqiang.zhu@yimifudao.com>
2019-02-26 12:04:30 +0800

    可不带文本提示

--- smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/creator/type/ILoadingDialogCreator.java
+++ smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/creator/type/ILoadingDialogCreator.java
@@ -3,7 +3,7 @@ package com.coder.zzq.smartshow.dialog.creator.type;
 public interface ILoadingDialogCreator extends INormalDialogCreator<ILoadingDialogCreator> {
     ILoadingDialogCreator message(CharSequence msg);
 
-    ILoadingDialogCreator withoutMsgTip();
+    ILoadingDialogCreator withNoMsgTip();
 
     ILoadingDialogCreator large();
 
--- smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/creator/type/impl/LoadingDialogCreator.java
+++ smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/creator/type/impl/LoadingDialogCreator.java
@@ -3,7 +3,6 @@ package com.coder.zzq.smartshow.dialog.creator.type.impl;
 import android.app.Dialog;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,8 +12,12 @@ import com.coder.zzq.smartshow.dialog.R;
 import com.coder.zzq.smartshow.dialog.creator.type.ILoadingDialogCreator;
 
 class LoadingDialogCreator extends NormalDialogCreator<ILoadingDialogCreator> implements ILoadingDialogCreator {
-    private CharSequence mMsg = "加载中...";
-    private boolean mWithoutMsgTip;
+    private CharSequence mMsg = "加载中";
+    public static final int BOX_SIZE_LARGE = 0;
+    public static final int BOX_SIZE_MIDDLE = 1;
+    public static final int BOX_SIZE_SMALL = 2;
+    private int mBoxSize = BOX_SIZE_LARGE;
+    private boolean mWithNoMsgTip;
     @DrawableRes
     private int mDrawablRes = R.drawable.smart_show_loading_img;
     @LayoutRes
@@ -34,31 +37,33 @@ class LoadingDialogCreator extends NormalDialogCreator<ILoadingDialogCreator> im
 
     @Override
     public ILoadingDialogCreator message(CharSequence msg) {
-        mMsg = Utils.isEmpty(msg) ? "加载中..." : msg;
+        mMsg = Utils.isEmpty(msg) ? "加载中" : msg;
         return this;
     }
 
     @Override
-    public ILoadingDialogCreator withoutMsgTip() {
-        mWithoutMsgTip = true;
+    public ILoadingDialogCreator withNoMsgTip() {
+        mWithNoMsgTip = true;
         return this;
     }
 
     @Override
     public ILoadingDialogCreator large() {
-        mLayoutRes = R.layout.smart_show_loading_large;
+        mBoxSize = BOX_SIZE_LARGE;
         return this;
     }
 
 
     @Override
     public ILoadingDialogCreator middle() {
-        mLayoutRes = R.layout.smart_show_loading_middle;
+        mBoxSize = BOX_SIZE_MIDDLE;
         return this;
     }
 
     @Override
     public ILoadingDialogCreator small() {
+        mBoxSize = BOX_SIZE_SMALL;
+        withNoMsgTip();
         mLayoutRes = R.layout.smart_show_loading_small;
         return this;
     }
@@ -66,11 +71,18 @@ class LoadingDialogCreator extends NormalDialogCreator<ILoadingDialogCreator> im
     @Override
     protected void initView(Dialog dialog, View dialogRootView) {
         super.initView(dialog, dialogRootView);
+        switch (mBoxSize) {
+            case BOX_SIZE_LARGE:
+                mLayoutRes = mWithNoMsgTip ? R.layout.smart_show_loading_large_no_tip : R.layout.smart_show_loading_large;
+                break;
+            case BOX_SIZE_MIDDLE:
+                mLayoutRes = mWithNoMsgTip ? R.layout.smart_show_loading_middle_no_tip : R.layout.smart_show_loading_middle;
+                break;
+        }
         Utils.inflate(mLayoutRes, (ViewGroup) dialogRootView, true);
         TextView msgView = dialogRootView.findViewById(R.id.smart_show_loading_message_view);
         if (msgView != null) {
             msgView.setText(mMsg);
-            msgView.setVisibility(TextUtils.isEmpty(mMsg) || mWithoutMsgTip ? View.GONE : View.VISIBLE);
         }
     }
 
