d744ca6b2057e9bfcb41cfedbe9f86bac64e5b6b
zhuzhiqiang <zhiqiang.zhu@yimifudao.com>
2019-03-15 10:51:49 +0800

    smart dialog 优化，增加dosomething 接口

--- smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/SmartDialog.java
+++ smart-dialog/src/main/java/com/coder/zzq/smartshow/dialog/SmartDialog.java
@@ -19,14 +19,15 @@ public final class SmartDialog {
 
     public static SmartDialog newInstance(IDialogCreator dialogCreator) {
         SmartDialog smartDialog = new SmartDialog();
-        smartDialog.mDialogCreator = dialogCreator;
+        smartDialog.mDialogCreator = Utils.requireNonNull(dialogCreator, "dialog creator can not null!");
         return smartDialog;
     }
 
     public boolean show(Activity activity) {
         if (!Utils.isUpdateActivityUIPermitted(activity) ||
                 (mNestedDialog == null && mDialogCreator == null)) {
-            EasyLogger.d("do nothing when conditions not available!");
+            EasyLogger.d("do nothing but recycle when conditions not available!");
+            recycle();
             return false;
         }
 
@@ -34,11 +35,11 @@ public final class SmartDialog {
             mNestedDialog = mDialogCreator.createDialog(activity);
             EasyLogger.d("create a new dialog:\n " + mNestedDialog);
         } else {
-            mDialogCreator.resetDialogPerShow(mNestedDialog);
             EasyLogger.d("reuse dialog:\n " + mNestedDialog);
         }
 
         if (mNestedDialog != null) {
+            mDialogCreator.resetDialogPerShow(mNestedDialog);
             try {
                 mNestedDialog.show();
                 return true;
@@ -55,6 +56,8 @@ public final class SmartDialog {
     public boolean dismiss(Activity activity) {
         if (!Utils.isUpdateActivityUIPermitted(activity) ||
                 (mNestedDialog == null || !mNestedDialog.isShowing())) {
+            EasyLogger.d("do nothing but recycle when conditions not available!");
+            recycle();
             return false;
         }
 
@@ -67,6 +70,11 @@ public final class SmartDialog {
         }
     }
 
+    private void recycle() {
+        mNestedDialog = null;
+        mDialogCreator = null;
+    }
+
     public SmartDialog reuse(boolean reuseDialog) {
         mReuseDialog = reuseDialog;
         return this;
@@ -74,5 +82,21 @@ public final class SmartDialog {
 
     public boolean isShowing() {
         return mNestedDialog != null && mNestedDialog.isShowing();
+    }
+
+    public boolean doSomething(Activity activity, DoSomethingCallback callback) {
+        if (!Utils.isUpdateActivityUIPermitted(activity) || mNestedDialog == null || callback == null) {
+            EasyLogger.d("do nothing but recycle when conditions not available!");
+            recycle();
+            return false;
+        }
+
+        callback.doSomething(mNestedDialog);
+
+        return true;
+    }
+
+    public interface DoSomethingCallback {
+        void doSomething(Dialog nestedDialog);
     }
 }
