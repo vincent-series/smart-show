bafa924965155410de60603f380720f6caf4f8e1
zhiqiang.zhu <zhiqiang.zhu@accenture.com>
2021-03-14 16:57:20 +0800

    add comment

--- smart-toast/src/main/java/com/coder/zzq/smartshow/toast/factory/BaseToastConfig.java
+++ smart-toast/src/main/java/com/coder/zzq/smartshow/toast/factory/BaseToastConfig.java
@@ -3,10 +3,12 @@ package com.coder.zzq.smartshow.toast.factory;
 import android.widget.Toast;
 
 public class BaseToastConfig {
+    //要显示的message
     public CharSequence mMsg = "";
     public int mDuration = Toast.LENGTH_SHORT;
     public int mGravity;
     public int mXOffset;
     public int mYOffset;
+    //退出当前页面时是否隐藏当前正在显示的Toast
     public boolean mCancelOnActivityExit;
 }
