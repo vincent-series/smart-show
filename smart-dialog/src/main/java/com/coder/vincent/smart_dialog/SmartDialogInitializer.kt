6ac7d44aec49f63499d7e6f5ef293e25d6b67767
zhuzq <zhiqiang.zhu@metawall.ai>
2023-06-12 10:58:32 +0800

    change the initializer provider authorities to avoid conflict

--- smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialogInitializer.kt
+++ smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialogInitializer.kt
@@ -1,10 +1,8 @@
 package com.coder.vincent.smart_dialog
 
 import android.content.Context
-import android.widget.Toast
 import androidx.startup.Initializer
 import com.coder.vincent.series.common_lib.CommonLibInitializer
-import com.coder.vincent.series.common_lib.application
 import com.coder.vincent.series.common_lib.vincentActivityLifecycleCallbacks
 
 class SmartDialogInitializer : Initializer<Unit> {
@@ -13,5 +11,5 @@ class SmartDialogInitializer : Initializer<Unit> {
     }
 
     override fun dependencies() =
-        mutableListOf<Class<out Initializer<*>>>(CommonLibInitializer::class.java)
+        listOf<Class<out Initializer<*>>>(CommonLibInitializer::class.java)
 }
