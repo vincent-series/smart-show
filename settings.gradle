b6c81079e4ae0380d7c614a9a4c35fed89213b9e
Vincent <vincent.king.chu@gmail.com>
2023-08-03 17:48:51 +0800

    upgrade gradle to 8.0

--- settings.gradle
+++ settings.gradle
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
