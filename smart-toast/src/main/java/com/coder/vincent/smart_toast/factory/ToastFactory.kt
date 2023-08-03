ca1e4616f27926eef7d3adc0adad96e77ffddce1
Vincent <vincent.king.chu@gmail.com>
2023-08-03 17:39:56 +0800

    remove applyNewConfig function in ToastFactory

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/factory/ToastFactory.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/factory/ToastFactory.kt
@@ -1,10 +1,7 @@
 package com.coder.vincent.smart_toast.factory
 
-import android.view.View
-import com.coder.vincent.smart_toast.CompactToast
-import com.coder.vincent.smart_toast.ToastConfig
+import com.coder.vincent.smart_toast.compact.CompactToast
 
-internal interface ToastFactory {
-    fun produceToast(config: ToastConfig): CompactToast?
-    fun applyNewConfig(toastView: View, config: ToastConfig)
+interface ToastFactory {
+    fun produceToast(config: ToastConfig): CompactToast
 }
