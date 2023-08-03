70a765a3ce57f5933dc0619bd3437f0f0fc4023e
Vincent <vincent.king.chu@gmail.com>
2023-08-03 17:37:18 +0800

    modify constants

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/Constants.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/Constants.kt
@@ -1,20 +1,21 @@
 package com.coder.vincent.smart_toast
 
 import android.graphics.Color
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.annotation.IntDef
-import com.coder.vincent.series.common_lib.application
+import com.coder.vincent.series.common_lib.Toolkit
 
-internal const val DEFAULT_VALUE = -1
-internal const val TOAST_TYPE_CLASSIC = "classic"
-internal const val TOAST_TYPE_EMOTION = "emotion"
-const val INTENT_KEY_BOUND_PAGE_ID = "startPageId"
-const val INTENT_KEY_PENDING_BOUND_PAGE_ID = "pendingPageId"
-internal val DEFAULT_TOAST_BACKGROUND_COLOR = Color.parseColor("#cc000000")
-internal val DEFAULT_TOAST_Y_OFFSET = Toast(application).yOffset
+internal const val TOAST_ALIAS_CLASSIC = "classic"
+internal const val TOAST_ALIAS_EMOTION = "emotion"
+val DEFAULT_TOAST_BACKGROUND_COLOR = Color.parseColor("#cc000000")
+val DEFAULT_TOAST_Y_OFFSET = Toast(Toolkit.context()).yOffset
 const val TOAST_ICON_POSITION_LEFT = 0
 const val TOAST_ICON_POSITION_RIGHT = 1
 
 @Retention(AnnotationRetention.BINARY)
 @IntDef(TOAST_ICON_POSITION_LEFT, TOAST_ICON_POSITION_RIGHT)
 annotation class IconPosition
+
+internal val handler = Handler(Looper.getMainLooper())
