90fa151f3f2fce3c72ee6fb4b879f5eed4fbf608
Vincent <vincent.king.chu@gmail.com>
2023-08-03 17:28:15 +0800

    rename toast visibility listener name

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/ViewAttachListener.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/ViewAttachListener.kt
@@ -2,13 +2,13 @@ package com.coder.vincent.smart_toast.compact
 
 import android.view.View
 
-internal class ViewAttachListener(private val visibilityChangedListener: ToastVisibilityChangedListener) :
+internal class ViewAttachListener(private val visibilityObserver: ToastVisibilityObserver) :
     View.OnAttachStateChangeListener {
     override fun onViewAttachedToWindow(v: View) {
-        visibilityChangedListener.onToastVisibilityChanged(v, true)
+        visibilityObserver.onToastVisibilityChanged(v, true)
     }
 
     override fun onViewDetachedFromWindow(v: View) {
-        visibilityChangedListener.onToastVisibilityChanged(v, false)
+        visibilityObserver.onToastVisibilityChanged(v, false)
     }
 }
