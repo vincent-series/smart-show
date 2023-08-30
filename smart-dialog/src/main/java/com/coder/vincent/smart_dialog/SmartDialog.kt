9cf52a8e27db4d8f8d00c384842bf593e8349ec1
Vincent <vincent.king.chu@gmail.com>
2023-08-30 12:16:32 +0800

    refactor SmartDialog entrance

--- smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialog.kt
+++ smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialog.kt
@@ -1,32 +1,45 @@
 package com.coder.vincent.smart_dialog
 
-import com.coder.vincent.smart_dialog.choose_list.ChooseListDialogInvoker
-import com.coder.vincent.smart_dialog.click_list.ClickListDialogInvoker
-import com.coder.vincent.smart_dialog.ensure.EnsureDialogInvoker
-import com.coder.vincent.smart_dialog.input_num.InputNumberDialogInvoker
-import com.coder.vincent.smart_dialog.input_text.InputTextDialogInvoker
-import com.coder.vincent.smart_dialog.loading.LoadingDialogInvoker
-import com.coder.vincent.smart_dialog.notification.NotificationDialogInvoker
+import com.coder.vincent.smart_dialog.choose_list.ChooseListDialogFacade
+import com.coder.vincent.smart_dialog.choose_list.ChooseListDialogImplementations
+import com.coder.vincent.smart_dialog.click_list.ClickListDialogFacade
+import com.coder.vincent.smart_dialog.click_list.ClickListDialogImplementations
+import com.coder.vincent.smart_dialog.ensure.EnsureDialogFacade
+import com.coder.vincent.smart_dialog.ensure.EnsureDialogImplementations
+import com.coder.vincent.smart_dialog.input_num.InputNumberDialogFacade
+import com.coder.vincent.smart_dialog.input_num.InputNumberDialogImplementations
+import com.coder.vincent.smart_dialog.input_text.InputTextDialogFacade
+import com.coder.vincent.smart_dialog.input_text.InputTextDialogImplementations
+import com.coder.vincent.smart_dialog.loading.LoadingDialogFacade
+import com.coder.vincent.smart_dialog.loading.LoadingDialogImplementations
+import com.coder.vincent.smart_dialog.notification.NotificationDialogFacade
+import com.coder.vincent.smart_dialog.notification.NotificationDialogImplementations
 
 object SmartDialog {
     @JvmStatic
-    fun notification() = NotificationDialogInvoker()
+    fun builderOfNotification(): NotificationDialogFacade.Builder =
+        NotificationDialogImplementations()
 
     @JvmStatic
-    fun ensure() = EnsureDialogInvoker()
+    fun builderOfEnsure(): EnsureDialogFacade.Builder = EnsureDialogImplementations()
 
     @JvmStatic
-    fun inputText() = InputTextDialogInvoker()
+    fun builderOfInputText(): InputTextDialogFacade.Builder =
+        InputTextDialogImplementations()
 
     @JvmStatic
-    fun inputNumber() = InputNumberDialogInvoker()
+    fun builderOfInputNumber(): InputNumberDialogFacade.Builder =
+        InputNumberDialogImplementations()
+
 
     @JvmStatic
-    fun clickList() = ClickListDialogInvoker()
+    fun builderOfClickList(): ClickListDialogFacade.Builder =
+        ClickListDialogImplementations()
 
     @JvmStatic
-    fun chooseList() = ChooseListDialogInvoker()
+    fun builderOfChooseList(): ChooseListDialogFacade.Builder =
+        ChooseListDialogImplementations()
 
     @JvmStatic
-    fun loading() = LoadingDialogInvoker()
+    fun builderOfLoading(): LoadingDialogFacade.Builder = LoadingDialogImplementations()
 }
