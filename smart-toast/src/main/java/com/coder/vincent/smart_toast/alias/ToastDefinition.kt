5a36d53fbc4ac558ec8f212362d35bbbd8123261
Vincent <vincent.king.chu@gmail.com>
2023-08-31 15:57:47 +0800

    add inflater argument for ToastView function

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/alias/ToastDefinition.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/alias/ToastDefinition.kt
@@ -1,9 +1,10 @@
 package com.coder.vincent.smart_toast.alias
 
+import android.view.LayoutInflater
 import android.view.View
 import com.coder.vincent.smart_toast.factory.ToastConfig
 
 interface ToastDefinition<Config : ToastConfig> {
-    fun toastView(): View
+    fun toastView(inflater: LayoutInflater): View
     fun applyConfig(toastView: View, config: Config)
 }
