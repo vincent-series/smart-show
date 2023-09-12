ed8aed081b96126162e36f059a0d47b18ff0f735
Vincent <vincent.king.chu@gmail.com>
2023-09-12 15:55:35 +0800

    rename some dialog alias

--- smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialog.kt
+++ smart-dialog/src/main/java/com/coder/vincent/smart_dialog/SmartDialog.kt
@@ -1,11 +1,11 @@
 package com.coder.vincent.smart_dialog
 
-import com.coder.vincent.smart_dialog.choose_list.ChooseListDialogFacade
-import com.coder.vincent.smart_dialog.choose_list.ChooseListDialogInvoker
-import com.coder.vincent.smart_dialog.click_list.ClickListDialogFacade
-import com.coder.vincent.smart_dialog.click_list.ClickListDialogInvoker
-import com.coder.vincent.smart_dialog.ensure.EnsureDialogFacade
-import com.coder.vincent.smart_dialog.ensure.EnsureDialogInvoker
+import com.coder.vincent.smart_dialog.choose_list.ChosenListDialogFacade
+import com.coder.vincent.smart_dialog.choose_list.ChosenListDialogInvoker
+import com.coder.vincent.smart_dialog.click_list.ClickedListDialogFacade
+import com.coder.vincent.smart_dialog.click_list.ClickedListDialogInvoker
+import com.coder.vincent.smart_dialog.ensure.AcknowledgeDialogFacade
+import com.coder.vincent.smart_dialog.ensure.AcknowledgeDialogInvoker
 import com.coder.vincent.smart_dialog.input_num.InputNumberDialogFacade
 import com.coder.vincent.smart_dialog.input_num.InputNumberDialogInvoker
 import com.coder.vincent.smart_dialog.input_text.InputTextDialogFacade
@@ -20,7 +20,7 @@ object SmartDialog {
     fun builderOfNotification(): NotificationDialogFacade.Builder = NotificationDialogInvoker()
 
     @JvmStatic
-    fun builderOfEnsure(): EnsureDialogFacade.Builder = EnsureDialogInvoker()
+    fun builderOfAcknowledge(): AcknowledgeDialogFacade.Builder = AcknowledgeDialogInvoker()
 
     @JvmStatic
     fun builderOfInputText(): InputTextDialogFacade.Builder = InputTextDialogInvoker()
@@ -29,10 +29,10 @@ object SmartDialog {
     fun builderOfInputNumber(): InputNumberDialogFacade.Builder = InputNumberDialogInvoker()
 
     @JvmStatic
-    fun builderOfClickList(): ClickListDialogFacade.Builder = ClickListDialogInvoker()
+    fun builderOfClickedList(): ClickedListDialogFacade.Builder = ClickedListDialogInvoker()
 
     @JvmStatic
-    fun builderOfChooseList(): ChooseListDialogFacade.Builder = ChooseListDialogInvoker()
+    fun builderOfChosenList(): ChosenListDialogFacade.Builder = ChosenListDialogInvoker()
 
     @JvmStatic
     fun builderOfLoading(): LoadingDialogFacade.Builder = LoadingDialogInvoker()
