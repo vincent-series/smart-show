17436d01fd2b3ef0962e1a6cd7bebbdad4b1f809
Vincent <vincent.king.chu@gmail.com>
2023-09-12 15:55:15 +0800

    move listener definition of input into Listeners.kt

--- smart-dialog/src/main/java/com/coder/vincent/smart_dialog/Listeners.kt
+++ smart-dialog/src/main/java/com/coder/vincent/smart_dialog/Listeners.kt
@@ -1,7 +1,6 @@
 package com.coder.vincent.smart_dialog
 
 import android.content.DialogInterface
-import androidx.appcompat.app.AppCompatDialog
 import com.coder.vincent.smart_dialog.choose_list.ChosenItem
 import com.coder.vincent.smart_dialog.click_list.ClickedItem
 
@@ -24,3 +23,7 @@ class DefaultCancelBtnListener : CancelBtnListener {
 typealias ItemClickedListener = (dialog: DialogInterface, clickedItem: ClickedItem) -> Unit
 
 typealias ItemChosenListener = (dialog: DialogInterface, chosenItems: List<ChosenItem>) -> Unit
+
+typealias InputTextConfirmListener = (dialog: DialogInterface, content: String) -> Unit
+
+typealias InputNumberConfirmListener = (dialog: DialogInterface, number: String) -> Unit
