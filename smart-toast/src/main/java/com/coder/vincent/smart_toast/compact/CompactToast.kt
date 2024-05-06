75f1dc4f1fdaf044b7b0409512aa16cbb4a41794
Vincent <vincent.king.chu@gmail.com>
2024-05-06 17:32:04 +0800

    define constant TOAST_DURATION_INDEFINITE

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/CompactToast.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/CompactToast.kt
@@ -5,6 +5,7 @@ import com.coder.vincent.smart_toast.factory.ToastConfig
 
 const val TOAST_DURATION_SHORT = 2000L
 const val TOAST_DURATION_LONG = 3500L
+const val TOAST_DURATION_INDEFINITE = 0L
 
 interface CompactToast {
     fun config(): ToastConfig
