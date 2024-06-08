13c8658efec325ba3980d71f100d93c02e3a73bb
Vincent <vincent.king.chu@gmail.com>
2024-06-08 19:32:11 +0800

    original toast just for android 11

--- smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/OriginalToast.kt
+++ smart-toast/src/main/java/com/coder/vincent/smart_toast/compact/OriginalToast.kt
@@ -1,27 +1,47 @@
 package com.coder.vincent.smart_toast.compact
 
-import android.annotation.SuppressLint
 import android.os.Build
-import android.os.Handler
 import android.view.View
 import android.widget.Toast
 import com.coder.vincent.series.common_lib.Toolkit
+import com.coder.vincent.smart_toast.bean.Duration
 import com.coder.vincent.smart_toast.factory.ToastConfig
-import java.lang.reflect.Field
 
-@Suppress("DEPRECATION")
-internal class OriginalToast(
-    toastView: View, config: ToastConfig, configApplyCallback: (View, ToastConfig) -> Unit
-) : AbsCompactToast(toastView, config, configApplyCallback) {
-    private val toast = Toast(Toolkit.context()).apply {
-        view = view()
-        duration =
-            if (config.duration.value.toLong() == TOAST_DURATION_LONG) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
-        setGravity(config().location.gravity, config().location.xOffset, config().location.yOffset)
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-            injectSafeHandler(this)
+
+internal class OriginalToast(val identity: View, var config: ToastConfig) : CompactToast {
+    private val toast: Toast =
+        Toast.makeText(
+            Toolkit.context(),
+            config.message,
+            if (config.duration.value >= Duration.LONG.value) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
+        )
+    private var visibilityObserver: ToastVisibilityObserver? = null
+    private var showing: Boolean = false
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            toast.addCallback(object : Toast.Callback() {
+                override fun onToastShown() {
+                    showing = true
+                    visibilityObserver?.onToastVisibilityChanged(identity, true)
+                }
+
+                override fun onToastHidden() {
+                    showing = false
+                    visibilityObserver?.onToastVisibilityChanged(identity, false)
+                }
+            })
         }
     }
+
+    override fun config(): ToastConfig = config
+
+    override fun updateConfig(config: ToastConfig) {
+        this.config = config
+    }
+
+    override fun view(): View = identity
+
 
     override fun show() {
         toast.show()
@@ -30,21 +50,15 @@ internal class OriginalToast(
     override fun cancel() {
         toast.cancel()
     }
-}
 
-@SuppressLint("SoonBlockedPrivateApi")
-private fun injectSafeHandler(toast: Toast) {
-    try {
-        val tnField: Field = Toast::class.java.getDeclaredField("mTN")
-        tnField.isAccessible = true
-        val tn = tnField[toast]
-        val handlerField = tn.javaClass.getDeclaredField("mHandler")
-        handlerField.isAccessible = true
-        val handlerOfTn = handlerField[tn] as Handler
-        handlerField[tn] = SafeHandler(handlerOfTn)
-    } catch (e: NoSuchFieldException) {
-        e.printStackTrace()
-    } catch (e: IllegalAccessException) {
-        e.printStackTrace()
+    override fun setVisibilityObserver(visibilityObserver: ToastVisibilityObserver) {
+        this.visibilityObserver = visibilityObserver;
     }
+
+    override fun removeVisibilityObserver(visibilityObserver: ToastVisibilityObserver) {
+        this.visibilityObserver = null
+    }
+
+    override fun isShowing(): Boolean = showing
+
 }
